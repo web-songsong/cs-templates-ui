e51746469b6b8634c2bdcef05414394a67446327
charmingsong <web_songsong@163.com>
2019-01-20 18:20:34 +0800

    init

--- meta.js
+++ meta.js
@@ -1,21 +1,9 @@
 module.exports = {
-  helpers: {
-    test(v1, v2, options) {
-      console.log(v1, v2, options)
-      return options.fn(this)
-    }
-  },
   prompts: {
     name: {
-      when: 'isNotTest',
-      type: 'string',
-      required: true,
-      message: 'Project name'
-    },
-    author: {
-      when: 'isNotTest',
-      type: 'string',
-      message: 'Author'
+      type: 'input',
+      message: '发版库的前缀（简称）',
+      default: 'my'
     }
   }
 }
